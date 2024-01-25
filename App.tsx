02b16767bc83f58f12abe445debd182a7932a570
Anthony Williams <anthony.williams.cs@gmail.com>
2024-01-24 16:03:05 -0800

    Convert app component to TS

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar'
+import { FC } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export const App: FC = () => {
+	return (
+		<View style={styles.container}>
+			<Text>📝 Edit App.tsx to work on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		alignItems: 'center',
+		backgroundColor: '#fff',
+		flex: 1,
+		justifyContent: 'center',
+	},
+})
+
+export default App
